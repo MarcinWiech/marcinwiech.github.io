112d5fb7ceac149f05e8144f5b027ffc4ad9078d
MarcinWiech <32527585+MarcinWiech@users.noreply.github.com>
2025-02-28 23:45:15 +0000

    Deploying to gh-pages from @ MarcinWiech/marcinwiech.github.io@9c7e1f8ac28611ff27993ae62e16d43c404cf91c 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,142 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "nav-photographs",
+          title: "photographs",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/photographs/";
+          },
+        },{id: "post-lucerne-steals-the-show",
+      
+        title: "Lucerne Steals the Show",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/switzerland/";
+        
+      },
+    },{id: "post-becoming-friends-with-running",
+      
+        title: "Becoming friends with running",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/running/";
+        
+      },
+    },{id: "post-devoxx-uk-2023",
+      
+        title: "Devoxx UK 2023",
+      
+      description: "The Developer Community Conference",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2023/devoxx/";
+        
+      },
+    },{id: "post-visiting-sopot",
+      
+        title: "Visiting Sopot",
+      
+      description: "One of the coolest towns in northern Poland",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2023/sopot/";
+        
+      },
+    },{id: "news-a-simple-inline-announcement",
+          title: 'A simple inline announcement.',
+          description: "",
+          section: "News",},{id: "news-a-long-announcement-with-details",
+          title: 'A long announcement with details',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
+            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-inspire',
+        title: 'Inspire HEP',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-rss',
+        title: 'RSS Feed',
+        section: 'Socials',
+        handler: () => {
+          window.open("/feed.xml", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-custom_social',
+        title: 'Custom_social',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.alberteinstein.com/", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
